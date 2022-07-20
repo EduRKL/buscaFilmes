0ea8c39de3cf5ca60a7811b057d3ac7b7ca5cdbf
Eduardo Lima <edulima282@gmail.com>
2022-07-19 21:35:32 -0300

    Consumindo uma API e iniciando gerador de figurinhas

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        // Fazer uma conexão HTTP (protocolo que a gente usa para acessar a web) e buscar os top 250 filmes
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        // correto: "https://imdb-api.com/en/API/Top250Movies/k_cisv2044"
+
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        
+
+        // Extrair só os dados que interessam (título, poster, classificação ou avaliação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFIlmes = parser.parse(body);
+        
+
+        // Exibir e manipular os dados como acharmos melhor
+        for (Map<String,String> filme : listaDeFIlmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+
+            
+        }
+
+
+
+
+
+    }
+}
